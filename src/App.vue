cc57685a3daa0c42262ab85225ff2337472e14ed
Yash Karle <yashkarle@gmail.com>
2018-06-25 02:00:03 +0530

    pass: have display and keyboard sections

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div class="calculator">
+    <div class="display">{{ display }}</div>
+    <div class="keyboard"></div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +10,33 @@ export default {
   name: 'app',
   data() {
     return {
-      message: 'Hello, Vue!'
+      display: 0
     };
   }
 };
 </script>
+
+<style scoped>
+.calculator {
+  width: 30rem;
+  max-width: 100%;
+  margin: 3rem auto;
+  padding: .3rem;
+}
+
+.calculator .display {
+  height: 4rem;
+  line-height: 4rem;
+  margin: 0 .3rem;
+  padding: 0 0;
+  text-align: right;
+  font-size: 1.5em;
+  color: #fff;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+}
+
+.calculator .keyboard {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
